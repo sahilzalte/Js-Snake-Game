8ce472648134d8149a18f66c0dd5a19e507a9826
Sahil Zalte <zaltesahil94@gmail.com>
2026-01-14 23:01:42 +0530

    Add basic snake game movement and rendering
    Implemented snake movement logic, rendering, and keyboard controls in script.js. Added .fill class and background color for snake segments in style.css.

--- script.js
+++ script.js
@@ -4,22 +4,14 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
+let intervalId = null;
 
-const blocks = []
+
+let blocks = []
 const snake = [{
     x: 1, y: 3
-}, {
-    x: 1, y: 4
-}, {
-    x: 1, y: 5
 }]
-
-
-// for (let i = 0; i < rows * cols; i++) {
-//     const block = document.createElement('div');
-//     block.classList.add('block');
-//     board.appendChild(block);
-// }
+let direction = 'down';
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
@@ -32,3 +24,63 @@ for (let row = 0; row < rows; row++) {
 
     }
 }
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add('fill');
+    })
+}
+
+intervalId = setInterval(() => {
+    let head = null
+
+    if (direction === 'left') {
+        head = { x: snake[0].x, y: snake[0].y - 1 };
+    }
+    else if (direction === 'right') {
+        head = { x: snake[0].x, y: snake[0].y + 1 };
+    }
+    else if (direction === 'up') {
+        head = { x: snake[0].x - 1, y: snake[0].y };
+    }
+    else if (direction === 'down') {
+        head = { x: snake[0].x + 1, y: snake[0].y };
+    }
+
+    if (head.x < 0 || head.y < 0 || head.x >= rows || head.y >= cols) {
+        alert('Game Over');
+        clearInterval(intervalId);
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+    })
+
+    snake.unshift(head);
+    snake.pop();
+
+    render()
+}, 300);
+
+
+addEventListener('keydown', (e) => {
+
+    if (e.key === 'ArrowUp' || e.key === 'w' || e.key === 'W') {
+        direction = 'up';
+    }
+    else if (e.key === 'ArrowDown' || e.key === 's' || e.key === 'S') {
+        direction = 'down';
+    }
+    else if (e.key === 'ArrowLeft' || e.key === 'a' || e.key === 'A') {
+        direction = 'left';
+    }
+    else if (e.key === 'ArrowRight' || e.key === 'd' || e.key === 'D') {
+        direction = 'right';
+    }
+
+})
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key);
+
+});
